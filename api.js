4d9954bb91f0239bbea9f3bc0da8316342053375
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-09 19:58:39 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://terabox-dl-arman.vercel.app/api?data=${id}`
+            `https://viper.xasena.me/api/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {
